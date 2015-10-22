b7d1e3c2320096c93a479d2706d14fb3a4fb7ead
Estiko <estiko@mediaqu.com>
2015-10-22 08:02:24 +0700

    Notification drawer: full swipe to switch detection (2/2)
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- src/com/android/settings/cyanogenmod/NotificationDrawer.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawer.java
@@ -36,11 +36,13 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private static final String UI_COLLAPSE_BEHAVIOUR = "notification_drawer_collapse_on_dismiss";
     private static final String PRE_SMART_PULLDOWN = "smart_pulldown";
     private static final String STATUS_BAR_CUSTOM_HEADER = "custom_status_bar_header";
+    private static final String SWIPE_TO_SWITCH_SCREEN_DETECTION = "full_swipe_to_switch_detection";
 
     private ListPreference mCollapseOnDismiss;
     private ListPreference mSmartPulldown;
     private Preference mHeadsUp;
     private CheckBoxPreference mStatusBarCustomHeader;
+    private CheckBoxPreference mFullScreenDetection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,6 +80,10 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1);
         mStatusBarCustomHeader.setOnPreferenceChangeListener(this);
 
+        mFullScreenDetection = (CheckBoxPreference) findPreference(SWIPE_TO_SWITCH_SCREEN_DETECTION);
+        mFullScreenDetection.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.SWIPE_TO_SWITCH_SCREEN_DETECTION, 0) == 1);
+        mFullScreenDetection.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -107,6 +113,11 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.System.putInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
+            return true;
+        } else if (preference == mFullScreenDetection) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.SWIPE_TO_SWITCH_SCREEN_DETECTION, value ? 1 : 0);
             return true;
         }
 
